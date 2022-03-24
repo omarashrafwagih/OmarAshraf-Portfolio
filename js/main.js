a003cae1e43a65d1e41bf7a6fa0ffac090e97ccb
Omar Ashraf <omar_ashraf_93@hotmail.com>
2022-03-24 15:05:49 +0300

    Added About me Section & Fixed Some Issues in Navbar and Hero Section

--- js/main.js
+++ js/main.js
@@ -37,10 +37,17 @@ musicBtn.addEventListener("click", () => {
 // Mobile Menu Button
 const mobileMenuBtn = document.getElementById("menu-btn");
 const myNav = document.querySelector("nav");
+const myNavLinks = document.querySelectorAll(".menu ul a");
 
 mobileMenuBtn.addEventListener("click", () => {
   myNav.classList.toggle("show");
   mobileMenuBtn.children[0].classList.toggle("fa-xmark");
   mobileMenuBtn.children[0].classList.toggle("fa-bars-staggered");
   document.body.classList.toggle("overflow-it");
+
+  myNavLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      mobileMenuBtn.click();
+    });
+  });
 });
